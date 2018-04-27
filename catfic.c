b8405c1b589bbc23a98d25d3f030071aa33e7e1b
Isabelle HESTIN <ihestin@e1r12p9.42.fr>
2018-04-27 16:39:56 +0200

    On ouvre le fichier en lecture et on le ferme

--- catfic.c
+++ catfic.c
@@ -1,9 +1,21 @@
 #include <unistd.h>
+#include <fcntl.h>
 
 int     main(int argc, char *argv[])
 {
+	int		fd;
+
 	if (argc != 2)
 	{
-		write(2,"Usage: catfic 'fichier a lister'");
+		write(2,"Usage: catfic 'fichier a lister'\n",32);
+		return (-1);
 	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		write(2, "invalid File.\n", 14);
+		return (-1);
+	}
+	close (fd);
+	return(0);
 }
